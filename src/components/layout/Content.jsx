1d49bf44b1b71c391ad1444330df97287171db29
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-03-16 21:41:13 -0300

    Rotas não encontradas

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -3,15 +3,17 @@ import { Routes, Route } from 'react-router-dom'
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 import "./Content.css"
 
 const Content = props => {
    return(
     <main className="Content">
-        <Routes>
+        <Routes>                        
             <Route path="/about" element={<About />} />
-            <Route path="/param/:id" element={<Param />} />
-            <Route path="/" element={<Home />} />
+            <Route path="/param/:id" element={<Param />} />            
+            <Route exact path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     </main>
    ) 
